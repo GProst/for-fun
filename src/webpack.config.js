4a4729d58017bfd849471499bd85cf182ae50f48
GProst <gprostmail@gmail.com>
2017-05-03 01:16:13 +0300

    Refactor app a little bit & update webpack conf

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -2,46 +2,60 @@
 
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackChunkHash = require("webpack-chunk-hash");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 const path = require("path");
 
 
 module.exports = function getConfig() {
-    return {
-        target: "web",
-        context: __dirname,
-        entry: {
-            "app": "./main.ts"
+  return {
+    target: "web",
+    context: __dirname,
+    entry: {
+      "app": "./main.ts"
+    },
+    cache: true,
+    devtool: "source-map",
+
+    resolve: {
+      extensions: [".ts", ".js", ".json"]
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.ts/,
+          use: ['awesome-typescript-loader', 'angular2-template-loader']
         },
-        cache: true,
-        devtool: "source-map",
-
-        resolve: {
-            extensions: [ ".ts", ".js", ".json"]
-        },
-
-        module: {
-            rules: [
-                {
-                    test: /\.ts/,
-                    use: ['awesome-typescript-loader', 'angular2-template-loader']
-                }
-            ]
-        },
-
-        plugins: [
-            new HtmlWebpackPlugin({
-                template: './index.html',
-                filename: 'index.html',
-                inject: 'body',
-                cashe: true,
-                showErrors: true
-            })
-        ],
-
-        output: {
-            path: path.join(__dirname, "../dist"),
-            filename: "[name].js",
-            chunkFilename: "[name].js"
+        {
+          test: /\.html/,
+          use: ['html-loader']
         }
-    };
+      ]
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        filename: 'index.html',
+        inject: 'body',
+        cashe: true,
+        showErrors: true
+      }),
+
+      new CleanWebpackPlugin(["dist"], {
+        root: path.join(__dirname, "../"),
+        verbose: true,
+        dry: false
+      }),
+
+      new WebpackChunkHash()
+    ],
+
+    output: {
+      path: path.join(__dirname, "../dist"),
+      filename: "[name].js",
+      chunkFilename: "[name].js"
+    }
+  };
 };
