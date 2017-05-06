bc811bb26a73684f460ef80edb654848ef58ef3a
GProst <gprostmail@gmail.com>
2017-05-06 19:05:46 +0300

    Add Angular Material & animations to the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdToolbarModule} from '@angular/material';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, BrowserAnimationsModule, MdToolbarModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
