95d422a7a6872d1697a889b0d0de1aa188144b2f
GProst <gprostmail@gmail.com>
2017-05-07 01:52:03 +0300

    Update style theme accent palette

--- src/app/angular-material/module.ts
+++ src/app/angular-material/module.ts
@@ -1,4 +1,4 @@
-import {NgModule}      from '@angular/core'
+import {NgModule} from '@angular/core'
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import {MdToolbarModule, MdButtonModule} from '@angular/material'
 
