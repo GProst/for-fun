dcd09701b4ad1fbf722cdb30b682527fdde96a63
GProst <gprostmail@gmail.com>
2017-05-10 01:58:47 +0300

    Refactor code style

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -9,77 +9,77 @@ const path = require("path");
 
 
 const config = {
-    target: "web",
-    context: __dirname,
-    entry: {
-        "app": "./main.ts"
-    },
-    cache: true,
-    devtool: "source-map",
+  target: "web",
+  context: __dirname,
+  entry: {
+    "app": "./main.ts"
+  },
+  cache: true,
+  devtool: "source-map",
 
-    resolve: {
-        extensions: [".ts", ".js", ".json"]
-    },
+  resolve: {
+    extensions: [".ts", ".js", ".json"]
+  },
 
-    module: {
-        rules: [
+  module: {
+    rules: [
+      {
+        test: /\.ts/,
+        use: ['awesome-typescript-loader', 'angular2-template-loader']
+      },
+      {
+        test: /\.html/,
+        use: ['html-loader']
+      },
+      {
+        test: /\.sass/,
+        use: ExtractTextPlugin.extract({
+          use: [
             {
-                test: /\.ts/,
-                use: ['awesome-typescript-loader', 'angular2-template-loader']
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
             },
-            {
-                test: /\.html/,
-                use: ['html-loader']
-            },
-            {
-                test: /\.sass/,
-                use: ExtractTextPlugin.extract({
-                    use: [
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                sourceMap: true
-                            }
-                        },
-                        'sass-loader'
-                    ],
-                    fallback: 'style-loader'
-                })
-            }
-        ]
-    },
+            'sass-loader'
+          ],
+          fallback: 'style-loader'
+        })
+      }
+    ]
+  },
 
-    plugins: [
-        new ExtractTextPlugin("styles.[contenthash].css"),
+  plugins: [
+    new ExtractTextPlugin("styles.[contenthash].css"),
 
-        new HtmlWebpackPlugin({
-            template: './index.html',
-            filename: 'index.html',
-            inject: 'body',
-            cashe: true,
-            showErrors: true
-        }),
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: 'index.html',
+      inject: 'body',
+      cashe: true,
+      showErrors: true
+    }),
 
-        new CleanWebpackPlugin(["dist"], {
-            root: path.join(__dirname, "../"),
-            verbose: true,
-            dry: false
-        }),
+    new CleanWebpackPlugin(["dist"], {
+      root: path.join(__dirname, "../"),
+      verbose: true,
+      dry: false
+    }),
 
-        new WebpackChunkHash()
-    ],
+    new WebpackChunkHash()
+  ],
 
-    output: {
-        path: path.join(__dirname, "../dist"),
-        filename: "[name].js",
-        chunkFilename: "[name].js"
-    }
+  output: {
+    path: path.join(__dirname, "../dist"),
+    filename: "[name].js",
+    chunkFilename: "[name].js"
+  }
 }
 
 config.devServer = {
-    port: 8080,
-    publicPath: config.output.publicPath,
-    historyApiFallback: true
+  port: 8080,
+  publicPath: config.output.publicPath,
+  historyApiFallback: true
 }
 
 module.exports = config
