daf2323da373e37bddcc583b08a8d28d01dccca1
GProst <gprostmail@gmail.com>
2017-05-09 15:31:30 +0300

    Create Routing module
    With Posts and About page routes

---
+++ src/app/routing/module.ts
@@ -0,0 +1,21 @@
+import {NgModule} from '@angular/core'
+import {RouterModule, Routes} from '@angular/router'
+
+import PostsPage from '../view/pages/Posts/component'
+import AboutPage from '../view/pages/About/component'
+
+const appRoutes: Routes = [
+  {path: 'posts', component: PostsPage},
+  {path: 'about', component: AboutPage},
+  {path: '**', redirectTo: '/posts', pathMatch: 'full'}
+]
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(appRoutes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export default class RoutingModule {}
