64062dd8bc5355b642eac4564ceef0fa4c5f154a
GProst <gprostmail@gmail.com>
2017-05-09 15:30:59 +0300

    Create View module (Pages module)
    With About & Posts pages

---
+++ src/app/view/pages/About/component.ts
@@ -0,0 +1,7 @@
+import {Component} from '@angular/core'
+
+@Component({
+  selector: 'gp-about-page',
+  template: require('./component.html')
+})
+export default class AboutPage {}
---
+++ src/app/view/pages/Posts/component.ts
@@ -0,0 +1,7 @@
+import {Component} from '@angular/core'
+
+@Component({
+  selector: 'gp-posts-page',
+  template: require('./component.html')
+})
+export default class PostsPage {}
