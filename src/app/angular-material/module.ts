226600c57a1cf813c56ec3c6b7dc747ab2deb19b
GProst <gprostmail@gmail.com>
2017-05-09 16:17:05 +0300

    Add Pages navigation via MdTabs

--- src/app/angular-material/module.ts
+++ src/app/angular-material/module.ts
@@ -1,9 +1,9 @@
 import {NgModule} from '@angular/core'
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
-import {MdToolbarModule, MdButtonModule} from '@angular/material'
+import {MdToolbarModule, MdButtonModule, MdTabsModule} from '@angular/material'
 
 @NgModule({
-  exports:  [BrowserAnimationsModule, MdToolbarModule, MdButtonModule],
-  imports:  [BrowserAnimationsModule, MdToolbarModule, MdButtonModule]
+  exports:  [BrowserAnimationsModule, MdToolbarModule, MdButtonModule, MdTabsModule],
+  imports:  [BrowserAnimationsModule, MdToolbarModule, MdButtonModule, MdTabsModule]
 })
 export class AngularMaterialModule {}
