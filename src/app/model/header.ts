7ece3d49f34e28c13a520df1bccceb0e93e9066b
GProst <gprostmail@gmail.com>
2017-05-10 02:00:18 +0300

    Replace header tabs content to model folder

---
+++ src/app/model/header.ts
@@ -0,0 +1,13 @@
+// TODO Type checking (interface)
+export default {
+  tabs: [
+    {
+      label: 'Posts',
+      path: '/posts'
+    },
+    {
+      label: 'About',
+      path: '/about'
+    }
+  ]
+}
