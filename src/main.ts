130423251bbde9cecd8dd3d59f2643367f93d490
GProst <gprostmail@gmail.com>
2017-05-14 20:46:14 +0300

    Configure app font style

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import 'core-js/client/shim.min'
 import 'zone.js'
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
 
+import './fonts'
 import './index.scss'
 
 import { AppModule } from './app/app.module'
