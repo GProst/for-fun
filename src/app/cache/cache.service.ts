7635cf5d0ac7bd73b619ad35ac9ecbe1b3f8b00a
GProst <gprostmail@gmail.com>
2017-06-02 00:18:27 +0300

    Create CacheService

---
+++ src/app/cache/cache.service.ts
@@ -0,0 +1,37 @@
+import {Injectable} from '@angular/core'
+
+import {PostData} from '../view/post/Post/component'
+import {PostCardData} from '../view/post-list/PostCard/component'
+
+type CachedPostsPage = {
+  pageNumber: number,
+  postList: Array<PostCardData>
+}
+
+@Injectable()
+export class CacheService {
+  private cachedPosts: Array<PostData> = []
+  private cachedPostsPages: Array<CachedPostsPage> = []
+
+  cachePost(post: PostData) {
+    if (this.cachedPosts.length > 5) { // do not cache too many
+      this.cachedPosts.shift()
+    }
+    this.cachedPosts.push(post)
+  }
+
+  getPost(slug: string) {
+    return this.cachedPosts.find(post => post.slug === slug)
+  }
+
+  cachePostsPage(pageNumber: number, postList: Array<PostCardData>) {
+    if (this.cachedPostsPages.length > 10) { // do not cache too many
+      this.cachedPostsPages.shift()
+    }
+    this.cachedPostsPages.push({pageNumber, postList})
+  }
+
+  getPostsPage(pageNumber: number) {
+    return this.cachedPostsPages.find(postsPage => postsPage.pageNumber === pageNumber)
+  }
+}
---
+++ src/app/cache/module.ts
@@ -0,0 +1,8 @@
+import {NgModule} from '@angular/core'
+
+import {CacheService} from './cache.service'
+
+@NgModule({
+  providers: [CacheService]
+})
+export class CacheModule {}
