d5e06ea82435041a7b36af469353d9dd90cb275e
GProst <gprostmail@gmail.com>
2017-06-05 19:36:49 +0300

    [WIP] Add RoutingModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,14 @@
 import {Component} from '@angular/core'
 
+import {RoutingService} from './routing/routing.service'
+
 @Component({
   selector: 'gp-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private routingService: RoutingService) {
+    /*we need to init routingService in root component, so it could subscribe to events ASAP and get initial route url*/
+  }
+}
