f588c2c0a9a084d27a39ad6ba7deaf5118c0b069
GProst <gprostmail@gmail.com>
2017-06-13 00:58:18 +0300

    Replace fake requests by server API requests

--- src/app/view/services/posts.service.ts
+++ src/app/view/services/posts.service.ts
@@ -4,7 +4,6 @@ import 'rxjs/add/operator/toPromise'
 
 import {PostCardData} from '../post-list/PostCard/component'
 import {PostData} from '../post/Post/component'
-import {postCards} from './fake-posts' // FixMe: fetch from backend
 
 import {CacheService} from '../../cache/cache.service'
 
@@ -12,22 +11,30 @@ type ArrayOfPostCards = Array<PostCardData>
 
 @Injectable()
 export class PostsService {
-  constructor(private cacheService: CacheService, private http: Http) {}
+  private options: RequestOptions
+
+  constructor(private cacheService: CacheService, private http: Http) {
+    const headers = new Headers({ 'Content-Type': 'application/json' })
+    this.options = new RequestOptions({headers})
+  }
 
   fetchPosts(pageNumber: number): Promise<ArrayOfPostCards> {
-    return new Promise((resolve) => {
-      this.cacheService.cachePagePosts(pageNumber, postCards)
-      setTimeout(() => resolve(postCards), 1000)
-    }) // TODO: server request
+    return this.http.get(`/api/posts/page/${pageNumber}`, this.options)
+      .toPromise()
+      .then((res: Response) => res.json().data)
+      .then((postCards: ArrayOfPostCards) => {
+        this.cacheService.cachePagePosts(pageNumber, postCards)
+        return postCards
+      })
+      .catch((err: Response | any) => {
+        throw err
+      })
   }
 
   fetchPost(slug: string): Promise<PostData> {
-    let headers = new Headers({ 'Content-Type': 'application/json' })
-    let options = new RequestOptions({headers})
-
-    return this.http.get(`/api/post/${slug}`, options)
+    return this.http.get(`/api/post/${slug}`, this.options)
       .toPromise()
-      .then((res: Response) => res.json())
+      .then((res: Response) => res.json().data)
       .then((post: PostData) => {
         this.cacheService.cachePost(post)
         return post
